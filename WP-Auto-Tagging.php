625dbbedd902c747761b781af904c1c5ebe9e5d7
smjunaidiqbal <smjunaidiqbal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-26 12:29:50 +0000

    Version 2.0 Pro Feature Enabled
    git-svn-id: https://plugins.svn.wordpress.org/wp-auto-tagging/trunk@1206739 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WP-Auto-Tagging.php
+++ WP-Auto-Tagging.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-Auto-Tagging
 Plugin URI: https://wordpress.org/plugins/wp-auto-tagging/
 Description: A Wordpress Plugin to Automatically Generate Tags for Posts and Pages.
-Version: 1.0
+Version: 2.0
 Author: Muhammad Junaid Iqbal
 Author URI: http://www.wallpapershdtop.com/
 License: GPL
@@ -30,11 +30,13 @@ $result = $conn->query($sql);
 //Counting SQL Records
 $count = $result->num_rows;
 //echo $count;
-
+//Limiting Returned Records
+$lmSQL = 'SELECT ID, post_title FROM '.$table_prefix.'posts WHERE LENGTH(post_title) > 5 AND post_status="publish" LIMIT 3000';
+$lmResult = $conn->query($lmSQL);
 //Looping Through Returened Records
-if ($result->num_rows > 0) {
+if ($result->num_rows >= 1) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
+    while($row = $lmResult->fetch_assoc()) {
         //echo  . $row["post_title"]. "<br>";
 		$post_ID = $row['ID'];
 		$post_title = $row['post_title'];
@@ -79,4 +81,69 @@ if ($result->num_rows > 0) {
 } 
 //Closing Database Connection
 $conn->close();
+
+//Adding Admin Hooks
+add_action('admin_menu', 'wp_auto_tagging_menu');
+ 
+function wp_auto_tagging_menu(){
+        add_menu_page( 'WP Auto Tagging Plugin', 'WP Auto Tagging', 'manage_options', 'wp-auto-tagging', 'init_watp_menu' );
+}
+ 
+function init_watp_menu(){
+		
+        echo "<h1>WP Auto Tagging</h1>";
+		
+		//Twitter Badge Code
+		echo '<a href="https://twitter.com/zebicute" class="twitter-follow-button" data-show-count="false">Follow @zebicute</a>';
+		echo "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
+		
+		echo '<h3><a href="http://www.chunoa.com/product/wp-auto-tagging-plugin/" target="_blank">Go Pro</a></h3>';
+		
+		//Plugins Features
+		echo '<ul>
+			<li class="li-tag">1. It will read post title and will split title into Tags and will add tags to all the posts automatically (posts which are missing tags).</li>
+			
+			<li class="li-tag">2. You do not need to manually insert tags for each post.</li>
+			
+			<li class="li-tag">3. Install WP Auto Tagging Wordpress Plugin, Activate it and tags will be added to all already published posts.</li>
+			
+			<li class="li-tag">4. WP Auto Tagging will add tags to new posts automatically as well (post you are going to publish now).</li>
+			
+			<li class="li-tag">5. WP Auto Tagging supports only One word tag. If you want to add tags with more than one word you can contact me at smjunaidiqbal@gmail.com</li>
+			
+			<li class="li-tag">6. Plugin will skip words in title less than 4 characters.</li>
+			
+			<li class="li-tag">7. Plugin will generate and add 6 tags for your published posts, you can delete unwanted tags and can update post.</li>
+			
+			<li class="li-tag">8. All of the generated and added tags will not be deleted once you de-activate WP Auto Tagging Wordpress plugin.</li>
+			
+		</ul>';
+		
+		echo "<h1>Free/ Pro Features</h1>";
+		echo '<ul>
+		<li class="li-tag">1. Plugin is free for your first 3000 posts and will add tags to them automatically.</li>
+		<li class="li-tag">2. You will need to purchase pro version of plugin if you have more than 3000 posts.</li>
+		<li class="li-tag">3. You will need to pay $99 for pro version of WP Auto Tagging Plugin.</li>
+		<li class="li-tag">4. Life time pro version of WP Auto Tagging plugin will let you to add tags for old as well as new posts.</li>
+		</ul>';
+		
+		echo '<h3><a href="http://www.chunoa.com/product/wp-auto-tagging-plugin/" target="_blank">Go Pro</a></h3>';
+		
+}
+
+//add_shortcode("superb", "test_process_shortcode");
+// 
+//function test_process_shortcode(){
+//echo '<form>';
+//echo '<p><label>Your Name<strong>*</strong><br>';
+//echo '<input type="text" size="48" name="name" value="one"></label></p>';
+//echo '<p><label>Email Address<strong>*</strong><br>';
+//echo '<input type="email" size="48" name="email" value="two"></label></p>';
+//echo '<p><label>Subject<br>';
+//echo '<input type="text" size="48" name="subject" value="three"></label></p>';
+//echo '<p><label>Enquiry<strong>*</strong><br>';
+//echo '<p><input type="submit" name="sendfeedback" value="Send Message"></p>';
+//echo '</form>';
+//}
+
 ?>
